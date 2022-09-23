ad2f5efab74660b555e6aec3ca74fca293729e96
Avd76 <113949901+Avd76@users.noreply.github.com>
2022-09-23 08:17:24 -0400

    Added environmental variables

--- database/pool.js
+++ database/pool.js
@@ -1,12 +1,12 @@
 const Pool = require("pg").Pool;
-require("dotenv").config
+require("dotenv").config()
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'KQTLOP23',
-    database: 'todo_database',
-    host: 'localhost',
-    port: 5432
+    user: proccess.env.PG_USER,
+    password: process.env.PG_PASS,
+    database: process.env.PG_DB,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT
 })
 
 module.exports = pool;
