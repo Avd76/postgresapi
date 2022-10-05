41bde59c400dbbdb718d233146188370dec5894b
Avd76 <113949901+Avd76@users.noreply.github.com>
2022-10-05 10:00:48 -0400

    Ready for deployment

--- server/database/pool.js
+++ server/database/pool.js
@@ -1,5 +1,5 @@
 const Pool = require("pg").Pool;
-require("dotenv").config()
+require("dotenv").config();
 
 const pool = new Pool({
     user: process.env.PG_USER,
@@ -7,6 +7,6 @@ const pool = new Pool({
     database: process.env.PG_DB,
     host: process.env.PG_HOST,
     port: process.env.PG_PORT
-})
+});
 
 module.exports = pool;
