2d31c81d4049cfa2e2140a50a079cb0057ddff51
Avd76 <113949901+Avd76@users.noreply.github.com>
2022-10-05 09:50:21 -0400

    Fixed error

--- server/database/pool.js
+++ server/database/pool.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 require("dotenv").config()
 
 const pool = new Pool({
-    user: proccess.env.PG_USER,
+    user: process.env.PG_USER,
     password: process.env.PG_PASS,
     database: process.env.PG_DB,
     host: process.env.PG_HOST,
